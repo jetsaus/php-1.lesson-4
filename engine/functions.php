35ae502e380d752c116b0a0ea16354ae79723349
jetsaus <jetsaus@gmail.com>
2019-06-14 22:19:39 +0800

    1.Создал структуру папок и разместил в ней файлы сайта ДЗ4.

---
+++ engine/functions.php
@@ -0,0 +1,32 @@
+<?php
+
+
+function render($file, $variables = [])
+{
+	if (!is_file($file)) {
+		echo 'Template file "' . $file . '" not found';
+		exit();
+	}
+
+	if (filesize($file) === 0) {
+		echo 'Template file "' . $file . '" is empty';
+		exit();
+	}
+
+
+	$templateContent = file_get_contents($file);
+
+	foreach ($variables as $key => $value) {
+		if (is_array($value)) {
+			continue;
+		}
+
+		$key = '{{' . strtoupper($key) . '}}';
+
+		$templateContent = str_replace($key, $value, $templateContent);
+	}
+
+	return $templateContent;
+}
+
+
