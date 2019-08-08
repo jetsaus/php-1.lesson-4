92195af2a41a1946fab22ef989eece023976341a
jetsaus <jetsaus@gmail.com>
2019-08-08 23:23:02 +0800

    2.2.Подключил конфигурационный файл и использовал из него константы с именеми каталогов.

--- public/index.php
+++ public/index.php
@@ -7,8 +7,9 @@
 <body>
     <h1>Фото-галерея</h1>
     <?php
-        // Подключим файл с модулем функций работы с галереей
-        require_once('../engine/gallery.php');
+        // Подключим конфигурацию и модуль работы с галереей
+        require_once('../config/config.php');
+        require_once(ENGINE_DIR . 'gallery.php');
         // Вызовем функцию вывода галереи
         fRendering('img', 3);
     ?>
