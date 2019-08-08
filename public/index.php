3007ee3a701b2c9d7932b862f809fa17459080eb
jetsaus <jetsaus@gmail.com>
2019-08-08 21:08:29 +0800

    2.1.Отсортировал файлы в привычном для человека порядке, для перебора использовал foreach

--- public/index.php
+++ public/index.php
@@ -8,9 +8,9 @@
     <h1>Фото-галерея</h1>
     <?php
         // Подключим файл с модулем функций работы с галереей
-        include_once('../engine/gallery.php');
+        require_once('../engine/gallery.php');
         // Вызовем функцию вывода галереи
-        fRendering('img', 5);
+        fRendering('img', 3);
     ?>
 </body>
 </html>
